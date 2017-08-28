c77e5b6dcc0efdff41d7a32d14c277dd7062083f
Charles Feng <charles@charles.io>
2017-08-27 20:45:20 -0700

    update MANIFEST

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='poynt',
-    version='0.0.6',
+    version='0.0.7',
     description='The official Poynt Python SDK.',
     long_description=__doc__,
     author='Charles Feng',
